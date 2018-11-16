9e7eb17267fc7d35b3cac776a6822e7bf6603388
linhongjing <linhongjing@weiboyi.com>
2018-11-16 09:51:00 +0800

    add the layout

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
+import './App.css'
 class App extends Component {
     render() {
-        return <div>
-            我
-            是一个页面
+        return <div id="jing-page-container">
+            <header id="header">我是头部</header>
+            <div id="page-content">
+                <div className="sidebar-left">
+                    我是做侧边栏
+                </div>
+                <div className="main-content"></div>
+                <div className="sidebar-right"></div>
+            </div>
+            <footer id="footer">
+                <div>我是页脚</div>
+            </footer>
         </div>
     }
 }
